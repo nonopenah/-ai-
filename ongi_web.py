f4c4b8d592dfa4a2b99486f9c5c2518a320eda95
naheekim <165535476+nonopenah@users.noreply.github.com>
2024-06-20 00:05:00 +0900

    Update ongi_web.py

--- ongi_web.py
+++ ongi_web.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 def run_streamlit():
     subprocess.Popen(['streamlit', 'run', 'ongi_bot.py'])
 
-@app.route('/')
-def index():
+@app.route('/') # 이 경로로 접근하면 아래의 index 함수가 호출
+def index(): #  HTML 문자열을 반환
     return render_template_string('''
         <h1>Flask에서 Streamlit 실행하기</h1>
         <iframe src="http://localhost:8501" width="100%" height="800"></iframe>
-    ''')
+    ''') # iframe 태그는 위 링크에 있는 streamlit 애플리케이션을 내장
 
 if __name__ == '__main__':
-    run_streamlit()
-    app.run(port=5000)
+    run_streamlit() # streamlit 애플리케이션 실행
+    app.run(port=5000) # flask 애플리케이션을 5000번 포트에서 실행
